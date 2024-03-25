d97e1edce0bc9f459ab593b29f36742984cfcc32
Nawat Pimolsathien <nawatpim@gmail.com>
2024-03-25 09:28:19 +0700

    PORT error

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const rateLimit = require('express-rate-limit');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
 //Load env vars
 dotenv.config({path:'./config/config.env' });
 
@@ -72,8 +76,6 @@ app.use('/api/v1/companies',companies);
 app.use('/api/v1/auth',auth);
 app.use('/api/v1/bookings',bookings);
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
